2c49429fdcb1de02d472a5a54a0fed8129d2b22a
youknowankit <thisisankityadav@gmail.com>
2026-07-24 07:11:05 -0700

    Add pino logging library to the project and logger.js to shared utils for its configuration

---
+++ src/shared/utils/logger.js
@@ -0,0 +1,19 @@
+import pino from "pino";
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+const logger = pino({
+  level: isDevelopment ? "debug" : "info",
+  transport: isDevelopment
+    ? {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
+          translateTime: "HH:MM:ss",
+          ignore: "pid,hostname",
+        },
+      }
+    : undefined,
+});
+
+export default logger;
